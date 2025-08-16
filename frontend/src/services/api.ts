2ef7e37123b85578a0db3fc7f56f21f164872022
renshangh <renshan@yahoo.com>
2025-08-16 06:51:40 -0500

    0.1

--- frontend/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:5057/api" });
-export const getProfile = () => api.get("/profile").then(r => r.data);
-export const getTransactions = () => api.get("/transactions").then(r => r.data);
-export const getInsights = () => api.get("/insights").then(r => r.data);
-export const acceptNudge = (type: string) => api.post("/nudge/accept", { type }).then(r => r.data);
-export const getYieldAlts = () => api.get("/yield/alternatives").then(r => r.data);
-
-export const fetchLLMNudge = () => api.get("/nudge/llm").then(r => r.data);
